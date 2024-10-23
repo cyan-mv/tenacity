605bd5a69366fc32d3a8835fff469ab8e57f5117
cyan-mv <cyan.mv@gmail.com>
2024-10-23 16:27:45 -0600

    feat(branch-resource/app-panel): add branch resource

---
+++ app/Filament/App/Resources/BranchResource.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Filament\App\Resources;
+
+use App\Filament\App\Resources\BranchResource\Pages;
+//use App\Filament\App\Resources\BranchResource\RelationManagers;
+use App\Models\Branch;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BranchResource extends Resource
+{
+    protected static ?string $model = Branch::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('branch_name')->required(),
+                TextInput::make('branch_phone')->required(),
+                TextInput::make('branch_address')->required(),
+                TextInput::make('branch_city')->required(),
+                TextInput::make('branch_country')->required(),
+                TextInput::make('branch_state')->required(),
+                Select::make('team_id')
+                    ->relationship('team', 'name')  // Assuming 'name' is a field in your Team model
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('branch_name'),
+                TextColumn::make('branch_phone'),
+                TextColumn::make('branch_address'),
+                TextColumn::make('branch_city'),
+                TextColumn::make('branch_country'),
+                TextColumn::make('team.name'),  // Show related team
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBranches::route('/'),
+            'create' => Pages\CreateBranch::route('/create'),
+            'edit' => Pages\EditBranch::route('/{record}/edit'),
+        ];
+    }
+}
