c4b0a7a06358132443ad8e7f2249858f6eb4d1fe
cyan-mv <cyan.mv@gmail.com>
2024-11-08 11:26:03 -0600

    feat(GroupResource/app):

---
+++ app/Filament/App/Resources/GroupResource.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Filament\App\Resources;
+
+use App\Filament\App\Resources\GroupResource\Pages;
+//use App\Filament\App\Resources\GroupResource\RelationManagers;
+use App\Models\Group;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class GroupResource extends Resource
+{
+    protected static ?string $model = Group::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-inbox-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('code')
+                    ->required()
+                    ->maxLength(3),
+                Forms\Components\TextInput::make('description')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('prefix')
+                    ->required()
+                    ->maxLength(3),
+                Forms\Components\Toggle::make('status')
+                    ->required(),
+                Forms\Components\TextInput::make('consecutive_length')
+                    ->required()
+                    ->numeric()
+                    ->default(5),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('code')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('description')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('prefix')
+                    ->searchable(),
+                Tables\Columns\IconColumn::make('status')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('consecutive_length')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListGroups::route('/'),
+            'create' => Pages\CreateGroup::route('/create'),
+            'edit' => Pages\EditGroup::route('/{record}/edit'),
+        ];
+    }
+}
