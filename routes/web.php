5cded68f3d8145865f2538d1ee0639f1d240a1e4
cyan-mv <cyan.mv@gmail.com>
2024-11-12 14:00:18 -0600

    Restore web.php and welcome.blade.php for Jetstream

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-use App\Http\Controllers\HomeController;
 
-Route::get('/', [HomeController::class, 'index'])->middleware('auth');
+Route::get('/', function () {
+    return view('welcome');
+});
