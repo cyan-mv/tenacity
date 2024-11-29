9b42e18b07be1a9260e3214fb1084e3677afe462
cyan-mv <cyan.mv@gmail.com>
2024-11-29 11:08:58 -0600

    refactor(group): simplify Group model by removing unused methods

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -23,12 +23,6 @@ class Group extends Model
         return $this->belongsTo(Company::class);
     }
 
-    // Optional: A helper method to generate the first card number
-    public function generateCardNumber()
-    {
-        return $this->prefix . $this->code . str_pad(1, $this->consecutive_length, '0', STR_PAD_LEFT);
-    }
-
     public function teams()
     {
         return $this->belongsToMany(Team::class, 'group_team', 'group_id', 'team_id');
@@ -58,29 +52,4 @@ class Group extends Model
             . $this->code
             . str_pad($this->current_sequence, $this->consecutive_length, '0', STR_PAD_LEFT);
     }
-
-    public function addClientToGroup(Client $client)
-    {
-        // Check if the client is already in the group
-        if ($this->clients()->where('client_id', $client->id)->exists()) {
-            throw new \Exception("Client is already in this group.");
-        }
-
-        // Generate a card number
-        $cardNumber = $this->generateNextCardNumber();
-
-        // Save the client-group relationship and the card number
-        $this->clients()->attach($client->id, ['card_number' => $cardNumber]);
-
-        return $cardNumber;
-    }
-
-    public function getCardNumbers()
-    {
-        return $this->clients()->withPivot('card_number')->get()->pluck('pivot.card_number');
-    }
-
-
-
-
 }
